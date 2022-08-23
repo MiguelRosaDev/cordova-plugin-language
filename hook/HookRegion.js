84ab65d817e96c8ea3e0d02ab012e752dfc8195d
miguelrosanos <miguel.rosa@moxie.pt>
2022-08-23 17:11:40 +0100

    hook

--- hook/HookRegion.js
+++ hook/HookRegion.js
@@ -20,7 +20,7 @@ module.exports = context => {
         return reject(err);
       }
       let infoPlist = plist.parse(data);
-      infoPlist.CFBundleDevelopmentRegion = 'Chinese';
+      infoPlist.CFBundleDevelopmentRegion = 'Portuguese';
       fs.writeFile(plistPath, plist.build(infoPlist), err => {
         if (err) {
           return reject();
